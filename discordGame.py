42218cd3ae03a6fcd59d3e7cadd18484364aecc8
iuyte <iuyte@gmail.com>
2017-05-13 13:48:15 -0400

    Removed redundant message logging

--- discordGame.py
+++ discordGame.py
@@ -3,22 +3,13 @@ import asyncio
 import pickle
 from game import *
 from game import data as game
-from tinydb import TinyDB, Query
 from datetime import datetime, timedelta
 
 with open('../discord_token.txt', 'r') as discord_file:
     DISCORD_TOKEN = discord_file.read()[:-1]
 prefix = ";"
 client = discord.Client()
-db = TinyDB("db/messageDB.json")
 game.load()
-
-def record(message, form):
-    db.insert({'type': form, 'id': message.id, 'server': message.server.id, 'channel': message.channel.id, 'author: ': message.author.id, 'content': message.content, 'time': str(datetime.now())})
-    servername = message.server.name
-    channelname = message.channel.name
-    authorname = message.author.name
-    print(servername.ljust(15) + " | " + channelname.ljust(15) + "  | " + authorname.ljust(10) + " | " + message.content)
 
 @client.event
 @asyncio.coroutine
@@ -31,7 +22,6 @@ def on_ready():
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    record(message, "create")
     content = message.content[1:]
     if message.content[0] == prefix:
         result = ""
@@ -64,15 +54,5 @@ def on_message(message):
             result = eval(content)
         if result != None and result != "":
             yield from client.send_message(message.channel, result)
-
-@client.event
-@asyncio.coroutine
-def on_message_delete(message):
-    record(message, "delete")
-
-@client.event
-@asyncio.coroutine
-def on_message_edit(message):
-    record(message, "edit")
 
 client.run(DISCORD_TOKEN)
