794809664830d7c5c492068479c1011613da6760
iuyte <iuyte@gmail.com>
2017-05-20 14:18:01 -0400

    work on remote updating

--- discordGame.py
+++ discordGame.py
@@ -22,8 +22,7 @@ def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------\n')
-    for server in client.servers:
-        yield from client.send_message(server.channels[0], "Connected")
+    yield from client.send_message(client.get_channel("315552571823489024"), "Connected")
 
 @client.event
 @asyncio.coroutine
@@ -92,15 +91,18 @@ def on_message(message):
             command = "sudo /sbin/reboot"
             subpcall(command, shell = True)
             result = "Rebooting..."
+        never = \
+"""
         elif content.startswith("update") and str(message.author.id) == "262949175765762050":
             command = "sudo su pi"
             subpcall(command, shell = True)
             command = "git pull"
             subpcall(command, shell = True)
-            sleep(30)
+            sleep(15)
             command = "sudo /sbin/reboot"
             subpcall(command, shell = True)
             result = "Updating..."
+"""
         elif str(message.author.id) == "262949175765762050":
             result = eval(content)
         elif "new" not in content:
